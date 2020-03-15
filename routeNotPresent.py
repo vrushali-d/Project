9a9523efc8478b8b6e28cb8449b22585ec27a226
vrushali-d <vrushali-d@github.com>
2020-03-15 11:41:40 +0530

    routesNotPresent.txt file lists routes which are in trips but not in route master and in route master but not in trips

--- routeNotPresent.py
+++ routeNotPresent.py
@@ -1,4 +1,14 @@
 #This program will find routes which are in one file but not in other file
+
+#This function will remove in between spaces of string
+def removeSpaceInBetween(string):
+	newStr=[]
+	for ch in string:
+		if not (ch == ' '):
+			newStr.append(ch)
+	return "".join(newStr)
+
+
 finTrip=open('./routeWiseTT.csv')
 finRoute=open('./routeIds.txt')
 
@@ -10,13 +20,19 @@ routeIdPrev=''
 cnt=0
 for line in finTrip:
 	tokens=line.split(',')
-	routeId=tokens[2].strip()+"-"+tokens[1][0].upper()
+	tokens[2]=removeSpaceInBetween(tokens[2])
+	if tokens[2][-1] == 'R':
+		if tokens[2][-2] == '-':
+			routeId=tokens[2][0:-1]+'R'
+		else:
+			routeId=tokens[2][0:-1]+'-R'
+	else:
+		routeId=tokens[2].strip()+"-"+tokens[1][0].upper()
 	if not(routeIdPrev == routeId):
 		listTrip.append(routeId)
 		routeIdPrev=routeId
 		cnt+=1
-print('Number of elements in list1:'+str(cnt))
-
+print('Number of elements in listTrip:'+str(cnt))
 
 listRoute=[]
 
@@ -28,13 +44,23 @@ finRoute.readline()
 cnt=0
 for line in finRoute:
 	listRoute.append(line[1:len(line)-1])
-	#print(line[0:len(line)-1])
 	cnt+=1
-#print(listRoute)
-print('Number of routes in list2:'+str(cnt))
+print('Number of routes in listRoute:'+str(cnt))
 
 #for every element of list1 check if it is in list 2
 #if not then save in list 3 and print also
+print("List of stops which are in trips but not in route master")
+
+cnt=0
+for route in listTrip:
+	if not (route in listRoute):
+		print(route)
+		cnt+=1
+
+print("Non present stops:"+str(cnt))
+
+
+print("List of stops which are in route Master  but not in trips")
 
 cnt=0
 for route in listRoute:
@@ -43,9 +69,5 @@ for route in listRoute:
 		cnt+=1
 
 print("Non present stops:"+str(cnt))
-
-
-
-
 
 
--- routeTripTT.py
+++ routeTripTT.py
@@ -1,6 +1,14 @@
 #A program to read csv file and come up with routeId and tripId
 #also start time and end time for that trip
 
+#This function will remove in between spaces of string
+def removeSpaceInBetween(string):
+	newStr=[]
+	for ch in string:
+		if not (ch == ' '):
+			newStr.append(ch)
+	return "".join(newStr)
+
 fin=open('./routeWiseTT.csv')
 fout=open('./RouteTrips.csv','w')
 fout.write("SrNo"+","+"RouteId"+","+"TripId"+","+"StartTime"+","+"End Time"+"\n")
@@ -13,10 +21,16 @@ for line in fin:
 	row=[];
 	tripCnt+=1;
 	cnt+=1
-#	if cnt > 200:
-#		break
-	tokens=line.split(',');
-	routeId=tokens[2].strip()+"-"+tokens[1][0].upper().strip()
+	tokens=line.split(',')
+	tokens[2]=removeSpaceInBetween(tokens[2])
+	if tokens[2][-1] == 'R':#if last letter is r then do not add 101-R-R
+		#routeId=tokens[2].strip()
+		if tokens[2][-2] == '-':
+			routeId=tokens[2][0:-1]+'R'
+		else:
+			routeId=tokens[2][0:-1]+'-R'
+	else:
+		routeId=tokens[2].strip()+"-"+tokens[1][0].upper().strip()
 	#If route id changes then start trip count from 1
 	if not(routeIdPrev == routeId):
 		tripCnt=1
@@ -34,6 +48,4 @@ for line in fin:
 	print(row)
 	fout.write(str(cnt)+","+row[0]+","+row[1]+","+row[2]+","+row[3]+"\n")
 	
-	#print(routeId)
-	#print(tokens)
-	#print('\n')
+
